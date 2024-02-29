eb2a2696e0c738d7f6941411655be69c6ac06247
Auric Vente <mprops@protonmail.com>
2024-02-29 08:18:40 -0600

    Mods

--- src/argparser.py
+++ src/argparser.py
@@ -44,7 +44,7 @@ class ArgParser:
     def get_list(self, attr: str, value: str, vtype: Any, separator: str) -> List[Any]:
         try:
             lst = list(map(vtype, map(str.strip, value.split(separator))))
-        except:
+        except BaseException:
             utils.exit(f"Failed to parse '--{attr}'")
             return []
 
@@ -100,7 +100,7 @@ class ArgParser:
                 num = int(num)
             elif vtype == float:
                 num = float(num)
-        except:
+        except BaseException:
             utils.exit(f"Failed to parse '{attr}'")
             return
 
